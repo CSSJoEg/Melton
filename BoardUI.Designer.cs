7053b43571bbcf2088231066aa49c37229512857
Eggerschwiler Joél <joel-eggerschwiler@hotmail.ch>
2022-12-15 12:07:10 +0100

    Projektdateien hinzufügen.

---
+++ BoardUI.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Melton
+{
+    partial class BoardUI
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(8, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(895, 1078);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // BoardUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 883);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "BoardUI";
+            this.Text = "BoardUI";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ BoardUI.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Melton
+{
+    public partial class BoardUI : Form
+    {
+        public BoardUI()
+        {
+            InitializeComponent();
+            ButtonArray();
+        }
+        public void ButtonArray()
+        {
+            for (int i = 0; i < 81; i++)
+            {
+                Button btn = new Button();
+                btn.Size = new Size(90, 90);
+                btn.Text = i.ToString();
+                btn.Click += btn_Click;
+                flowLayoutPanel1.Controls.Add(btn);
+            }
+        }
+        public void btn_Click(object sender, EventArgs e)
+        {
+            Button btnPos = (Button)sender;
+            if (btnPos.Tag == "")
+            {
+
+            }
+        }
+    }
+}
