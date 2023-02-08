2a479af281b2e3c2938f6d732bb0bc72bba7feac
Eggerschwiler Joél <joel-eggerschwiler@hotmail.ch>
2023-02-08 10:13:44 +0100

    Themes in Settings Semi-Working

--- Settings.Designer.cs
+++ Settings.Designer.cs
@@ -28,99 +28,105 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
-            this.Helligkeit = new System.Windows.Forms.Label();
-            this.Darkmode = new System.Windows.Forms.Label();
-            this.Layouts = new System.Windows.Forms.Label();
-            this.change_layout = new System.Windows.Forms.Button();
-            this.DarkmodeOn = new System.Windows.Forms.CheckBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.SuspendLayout();
-            // 
-            // Helligkeit
-            // 
-            this.Helligkeit.AutoSize = true;
-            this.Helligkeit.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Helligkeit.Location = new System.Drawing.Point(129, 205);
-            this.Helligkeit.Name = "Helligkeit";
-            this.Helligkeit.Size = new System.Drawing.Size(292, 73);
-            this.Helligkeit.TabIndex = 0;
-            this.Helligkeit.Text = "Helligkeit";
-            // 
-            // Darkmode
-            // 
-            this.Darkmode.AutoSize = true;
-            this.Darkmode.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Darkmode.Location = new System.Drawing.Point(129, 358);
-            this.Darkmode.Name = "Darkmode";
-            this.Darkmode.Size = new System.Drawing.Size(328, 73);
-            this.Darkmode.TabIndex = 1;
-            this.Darkmode.Text = "Darkmode";
-            // 
-            // Layouts
-            // 
-            this.Layouts.AutoSize = true;
-            this.Layouts.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Layouts.Location = new System.Drawing.Point(129, 509);
-            this.Layouts.Name = "Layouts";
-            this.Layouts.Size = new System.Drawing.Size(258, 73);
-            this.Layouts.TabIndex = 2;
-            this.Layouts.Text = "Layouts";
-            // 
-            // change_layout
-            // 
-            this.change_layout.Location = new System.Drawing.Point(768, 509);
-            this.change_layout.Name = "change_layout";
-            this.change_layout.Size = new System.Drawing.Size(176, 67);
-            this.change_layout.TabIndex = 3;
-            this.change_layout.Text = "change_layout";
-            this.change_layout.UseVisualStyleBackColor = true;
-            // 
-            // DarkmodeOn
-            // 
-            this.DarkmodeOn.AutoSize = true;
-            this.DarkmodeOn.Location = new System.Drawing.Point(768, 402);
-            this.DarkmodeOn.Name = "DarkmodeOn";
-            this.DarkmodeOn.Size = new System.Drawing.Size(176, 29);
-            this.DarkmodeOn.TabIndex = 4;
-            this.DarkmodeOn.Text = "Darkmode On";
-            this.DarkmodeOn.UseVisualStyleBackColor = true;
-            this.DarkmodeOn.CheckedChanged += new System.EventHandler(this.DarkmodeOn_CheckedChanged);
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(768, 241);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(176, 31);
-            this.textBox1.TabIndex = 5;
-            // 
-            // Settings
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1600, 865);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.DarkmodeOn);
-            this.Controls.Add(this.change_layout);
-            this.Controls.Add(this.Layouts);
-            this.Controls.Add(this.Darkmode);
-            this.Controls.Add(this.Helligkeit);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
-            this.Name = "Settings";
-            this.Text = "Settings";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Settings));
+			this.Helligkeit = new System.Windows.Forms.Label();
+			this.Themes = new System.Windows.Forms.Label();
+			this.Layouts = new System.Windows.Forms.Label();
+			this.change_layout = new System.Windows.Forms.Button();
+			this.Light_Theme = new System.Windows.Forms.CheckBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// Helligkeit
+			// 
+			this.Helligkeit.AutoSize = true;
+			this.Helligkeit.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Helligkeit.Location = new System.Drawing.Point(86, 131);
+			this.Helligkeit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Helligkeit.Name = "Helligkeit";
+			this.Helligkeit.Size = new System.Drawing.Size(182, 46);
+			this.Helligkeit.TabIndex = 0;
+			this.Helligkeit.Text = "Helligkeit";
+			// 
+			// Themes
+			// 
+			this.Themes.AutoSize = true;
+			this.Themes.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Themes.Location = new System.Drawing.Point(86, 229);
+			this.Themes.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Themes.Name = "Themes";
+			this.Themes.Size = new System.Drawing.Size(163, 46);
+			this.Themes.TabIndex = 1;
+			this.Themes.Text = "Themes";
+			// 
+			// Layouts
+			// 
+			this.Layouts.AutoSize = true;
+			this.Layouts.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Layouts.Location = new System.Drawing.Point(86, 326);
+			this.Layouts.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+			this.Layouts.Name = "Layouts";
+			this.Layouts.Size = new System.Drawing.Size(160, 46);
+			this.Layouts.TabIndex = 2;
+			this.Layouts.Text = "Layouts";
+			// 
+			// change_layout
+			// 
+			this.change_layout.Location = new System.Drawing.Point(512, 326);
+			this.change_layout.Margin = new System.Windows.Forms.Padding(2);
+			this.change_layout.Name = "change_layout";
+			this.change_layout.Size = new System.Drawing.Size(117, 43);
+			this.change_layout.TabIndex = 3;
+			this.change_layout.Text = "change_layout";
+			this.change_layout.UseVisualStyleBackColor = true;
+			// 
+			// Light_Theme
+			// 
+			this.Light_Theme.AutoSize = true;
+			this.Light_Theme.Location = new System.Drawing.Point(512, 257);
+			this.Light_Theme.Margin = new System.Windows.Forms.Padding(2);
+			this.Light_Theme.Name = "Light_Theme";
+			this.Light_Theme.Size = new System.Drawing.Size(103, 20);
+			this.Light_Theme.TabIndex = 4;
+			this.Light_Theme.Text = "Light Theme";
+			this.Light_Theme.UseVisualStyleBackColor = true;
+			this.Light_Theme.CheckedChanged += new System.EventHandler(this.Light_Theme_CheckedChanged_1);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(512, 154);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(119, 22);
+			this.textBox1.TabIndex = 5;
+			// 
+			// Settings
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1067, 554);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.Light_Theme);
+			this.Controls.Add(this.change_layout);
+			this.Controls.Add(this.Layouts);
+			this.Controls.Add(this.Themes);
+			this.Controls.Add(this.Helligkeit);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "Settings";
+			this.Text = "Settings";
+			this.ResumeLayout(false);
+			this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Label Helligkeit;
-        private System.Windows.Forms.Label Darkmode;
+        private System.Windows.Forms.Label Themes;
         private System.Windows.Forms.Label Layouts;
         private System.Windows.Forms.Button change_layout;
-        private System.Windows.Forms.CheckBox DarkmodeOn;
+        private System.Windows.Forms.CheckBox Light_Theme;
         private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Settings.cs
+++ Settings.cs
@@ -14,6 +14,9 @@ namespace Melton
 {
     public partial class Settings : Form
     {
+        Dictionary<ThemeColor, Color> Light;
+        Dictionary<ThemeColor, Color> Nature;
+        Dictionary<ThemeColor, Color> Dark;
         enum ThemeColor
         {
             Primary,
@@ -23,44 +26,48 @@ namespace Melton
         }
         void ChangeTheme(Color primaryColor, Color secondaryColor, Color tertiaryColor)
         {
-
+            Light = new Dictionary<ThemeColor, Color>() {
+            { ThemeColor.Primary, Color.WhiteSmoke },
+            { ThemeColor.Secondary, Color.Silver },
+            { ThemeColor.Tertiary, Color.White },
+            };
+            Nature = new Dictionary<ThemeColor, Color>() {
+            { ThemeColor.Primary, Color.DarkSeaGreen },
+            { ThemeColor.Secondary, Color.AliceBlue },
+            { ThemeColor.Tertiary, Color.Honeydew },
+            };
+            Dark = new Dictionary<ThemeColor, Color>() {
+            { ThemeColor.Primary, Color.DimGray },
+            { ThemeColor.Secondary, Color.DimGray },
+            { ThemeColor.Tertiary, Color.Black },
+            };
+        }
+        void ChangeTextColor(Color textcolor)
+		{
+            Light = new Dictionary<ThemeColor, Color>() {
+            { ThemeColor.Text, Color.Black }
+            };
+            Nature = new Dictionary<ThemeColor, Color>() {
+            { ThemeColor.Text, Color.Black }
+            };
+            Dark = new Dictionary<ThemeColor, Color>() {
+            { ThemeColor.Text, Color.White }
+            };
         }
         public Form parent { get; set; }
         public Settings(Form mdiParent)
         {
             InitializeComponent();
             parent = mdiParent;
-            Light = new Dictionary<ThemeColor, Color>() {
-            { ThemeColor.Primary, Color.WhiteSmoke },
-            { ThemeColor.Secondary, Color.Silver },
-            { ThemeColor.Tertiary, Color.White },
-            { ThemeColor.Text, Color.Black }
-            };
-            Nature = new Dictionary<ThemeColor, Color>() {
-            { ThemeColor.Primary, Color.DarkSeaGreen },
-            { ThemeColor.Secondary, Color.AliceBlue },
-            { ThemeColor.Tertiary, Color.Honeydew },
-            { ThemeColor.Text, Color.Black }
-            };
-            Dark = new Dictionary<ThemeColor, Color>() {
-            { ThemeColor.Primary, Color.DimGray },
-            { ThemeColor.Secondary, Color.DimGray },
-            { ThemeColor.Tertiary, Color.Black },
-            { ThemeColor.Text, Color.White }
-            };
-            Dictionary<ThemeColor, Color> Light = new Dictionary<ThemeColor, Color>();
-            Dictionary<ThemeColor, Color> Nature = new Dictionary<ThemeColor, Color>();
-            Dictionary<ThemeColor, Color> Dark = new Dictionary<ThemeColor, Color>();
         }
-        
-        private void DarkmodeOn_CheckedChanged(object sender, EventArgs e)
-        {
-            if (DarkmodeOn.Checked)
+		private void Light_Theme_CheckedChanged_1(object sender, EventArgs e)
+		{
+            if (Light_Theme.Checked)
             {
                 ChangeTheme(Light[ThemeColor.Primary], Light[ThemeColor.Secondary], Light[ThemeColor.Tertiary]);
                 ChangeTextColor(Light[ThemeColor.Text]);
             }
         }
-    }
+	}
 }
 
